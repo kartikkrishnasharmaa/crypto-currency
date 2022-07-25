5ab58140d890bc64026a45ab8c93d2351737f7e0
kartikkrishnasharmaa <kartikkrishnasharmaa@gmail.com>
2022-07-25 22:15:36 +0530

    cryptoapp

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import { useEffect,useState } from "react";
+import Axios from "axios";
+import Coin from "./components/Coin";
+
+function App() {
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+
+
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit10").then(
+      (response) =>{
+      setListOfCoins(response.data.coins);
+    })
+  }, []);
+
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  }); 
+
+ return(
+  <div className="App">
+  <div className="cryptoHeader">
+  <div className="h">Search Your CryptoCurrency</div>
+    <input
+      type="text"
+      placeholder="Bitcoin..."
+      onChange={(event) => {
+        setSearchWord(event.target.value);
+      }}
+    />
+  </div>
+              <div className="cryptoDisplay">
+              {filteredCoins.map((coin) => {
+                return (<Coin
+                  name={coin.name}
+                  icon={coin.icon}
+                  price={coin.price}
+                  symbol={coin.symbol}
+                />)
+              })}</div>
+          </div>
+ );
+}
+export default App;
